102ee4f9a6151a934355a193fc9981443d2df5d7
Kunj Meghapara <kunjmeghapara777@gmail.com>
2026-07-23 14:40:44 +0530

    changes in the final program

--- src/Day_1_2.java
+++ src/Day_1_2.java
@@ -1,97 +1,72 @@
-interface user{
-    void showRole();
+import java.util.ArrayList;
+import java.util.List;
+
+interface lib_items{
+    void ShowDetail();
 }
 
-/*
-Reason to choose interface over abstract class : in the current scenario user, borrower and librarian are having one common implementation which is showRole().
-if they had some common sharing data then we can go for the abstract class since here only one method is being overloaded thats why it is better to use interface.
- */
-
-class Book{
-    private String book_name;
-    private String serial_number;
+class Library{
+    private String name;
+    private List<lib_items> list;
+    Library(String name,List<lib_items> list){
+        this.name = name;
+        this.list = list;
+    }
+}
+class Book implements lib_items{
+    private String title;
     private String author;
-    Book(String book_name, String serial_number, String author){
-        this.book_name = book_name;
-        this.serial_number = serial_number;
-        this.author = author;
-    }
+    private String sr_no;
+    private boolean available;
 
-    public void displayBook(){
-        System.out.println("Book Name : " + book_name);
-        System.out.println("Serial No : " + serial_number);
-        System.out.println("Author    : " + author);
-    }
-}
-class Borrower implements user{
-    private String name;
-    private String contact;
-
-    Borrower(String name,String number){
-        if(number.length() != 10){
-            System.out.println("Invalid number format");
+    Book(String title,String author,String sr_no,boolean available){
+        if(sr_no.length() != 6){
+            System.out.println("Invalid Serial number !");
         }else {
-            this.name = name;
-            this.contact = number;
+            this.title = title;
+            this.author = author;
+            this.sr_no = sr_no;
+            this.available = available;
         }
     }
-    public void displayBorrower(){
-        System.out.println("Borrower Name : " + name);
-        System.out.println("Contact       : " + contact);
-    }
-
     @Override
-    public void showRole() {
-        System.out.println("The role of " + this.name + " is Borrower");
+    public void ShowDetail() {
+        System.out.println("Book Details are as follow : ");
+        System.out.println("Book Name      :  "+this.title);
+        System.out.println("Author name    :  "+this.author);
+        System.out.println("Serial number  :  "+this.sr_no);
+        System.out.println("Status         :  "+((this.available) ? "Available" : "Not available"));
     }
 }
-class Librarian implements user{
-    private String name;
-    private String contact;
-    Librarian(String name,String contact){
-        if(contact.length() != 10){
-            System.out.println("Invalid number format");
+class Novel implements lib_items{
+    private String title;
+    private String author;
+    private String sr_no;
+    private boolean available;
+
+    Novel(String title,String author,String sr_no,boolean available){
+        if(sr_no.length() != 6){
+            System.out.println("Invalid Serial number !");
         }else {
-            this.name = name;
-            this.contact = contact;
+            this.title = title;
+            this.author = author;
+            this.sr_no = sr_no;
+            this.available = available;
         }
     }
-    public void displayLibrarian(){
-        System.out.println("Librarian Name : " + name);
-        System.out.println("Contact        : " + contact);
-    }
     @Override
-    public void showRole() {
-        System.out.println("The role of " + this.name + " is Librarian");
+    public void ShowDetail() {
+        System.out.println("Novel Details are as follow : ");
+        System.out.println("Novel Name      :  "+this.title);
+        System.out.println("Author name     :  "+this.author);
+        System.out.println("Serial number   :  "+this.sr_no);
+        System.out.println("Status          :  "+((this.available) ? "Available" : "Not available"));
     }
 }
-class Day_1_2 {
-public static void main(String[] args) {
-    Book book = new Book(
-            "Java: The Complete Reference",
-            "B001",
-            "......"
-    );
 
-    Borrower borrower = new Borrower(
-            "Kunj Meghapara",
-            "9876543210"
-    );
-
-    Librarian librarian = new Librarian(
-            "Mr. Patel",
-            "9123456789"
-    );
-
-    System.out.println("===== BOOK DETAILS =====");
-    book.displayBook();
-
-    System.out.println("\n===== BORROWER DETAILS =====");
-    borrower.displayBorrower();
-    borrower.showRole();
-
-    System.out.println("\n===== LIBRARIAN DETAILS =====");
-    librarian.displayLibrarian();
-    librarian.showRole();
-}
+public class Day_1_2 {
+    public static void main(String[] args) {
+        List<lib_items> list = new ArrayList<>();
+        Library lib = new Library("Public library",list);
+    }
 }
