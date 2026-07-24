21b814e824fc9c08035089b36677904cf4d03095
Kunj Meghapara <kunjmeghapara777@gmail.com>
2026-07-24 16:35:16 +0530

    ..

--- src/Day_3.java
+++ src/Day_3.java
@@ -7,12 +7,13 @@ public class Day_3 {
     public static void main(String[] args) {
 
         ArrayList<lib_items> list = new ArrayList<>();
-        HashMap<String, lib_items> map = new HashMap<>();
         HashSet<String> authors = new HashSet<>();
+        HashMap<String, lib_items> map = new HashMap<>();
 
         Library lib = new Library("Public Library", list);
 
         for (int i = 1; i <= 250; i++) {
+
             Book b = new Book(
                     "Book " + i,
                     "Author " + (i % 20),
@@ -21,11 +22,12 @@ public class Day_3 {
             );
 
             list.add(b);
-            map.put(b.getSr_no(), b);
             authors.add(b.getAuthor());
+            map.put(b.getSr_no(), b);
         }
 
         for (int i = 1; i <= 250; i++) {
+
             Novel n = new Novel(
                     "Novel " + i,
                     "Author " + (i % 20),
@@ -34,18 +36,34 @@ public class Day_3 {
             );
 
             list.add(n);
-            map.put(n.getSr_no(), n);
             authors.add(n.getAuthor());
+            map.put(n.getSr_no(), n);
         }
 
+        System.out.println("Library : Public Library");
         System.out.println("Total Items : " + list.size());
         System.out.println("Unique Authors : " + authors.size());
 
+        Book newBook = new Book(
+                "Atomic Habits",
+                "James Clear",
+                "BK9999",
+                true
+        );
+
+        list.add(newBook);
+        authors.add(newBook.getAuthor());
+        map.put(newBook.getSr_no(), newBook);
+
+        System.out.println("\nBK9999 Added Successfully.");
+
         lib_items removed = map.remove("BK0100");
+
         if (removed != null) {
             list.remove(removed);
-            System.out.println("BK0100 Removed.");
+            System.out.println("BK0100 Removed Successfully.");
         }
+
 
         String search = "NV0200";
         boolean found = false;
@@ -53,22 +71,28 @@ public class Day_3 {
         /*
          * Slow Search (O(n))
          * Checks every item one by one.
-         * Faster way: HashMap because lookup by key is O(1) on average.
+         * Faster way: Use HashMap because searching by key
+         * takes O(1) time on average.
          */
+
+        System.out.println("\nLinear Search:");
 
         for (lib_items item : list) {
 
             if (item instanceof Book) {
+
                 Book b = (Book) item;
+
                 if (b.getSr_no().equals(search)) {
                     b.ShowDetail();
                     found = true;
                     break;
                 }
-            }
 
-            if (item instanceof Novel) {
+            } else if (item instanceof Novel) {
+
                 Novel n = (Novel) item;
+
                 if (n.getSr_no().equals(search)) {
                     n.ShowDetail();
                     found = true;
@@ -81,14 +105,20 @@ public class Day_3 {
             System.out.println("Item Not Found");
         }
 
-        System.out.println("\nHashMap Search");
+        System.out.println("\nHashMap Search:");
 
-        lib_items item = map.get(search);
+        lib_items result = map.get(search);
 
-        if (item != null) {
-            item.ShowDetail();
+        if (result != null) {
+            result.ShowDetail();
         } else {
             System.out.println("Item Not Found");
         }
+
+
+        System.out.println("\nComparison:");
+        System.out.println("1. ArrayList stores all library items and maintains insertion order.");
+        System.out.println("2. HashSet stores unique author names and automatically removes duplicates.");
+        System.out.println("3. HashMap stores serial numbers as keys, allowing much faster searching than an ArrayList.");
     }
 }
